e1552350de56b3420ae693d734b1cd3037859716
Avi-Ay <73743798+Avi-Ay@users.noreply.github.com>
2021-08-21 20:24:44 +0300

    new subfunction

---
+++ Beyonce.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+
+def show_pic():
+    image = Image.open("answer.png")
+    root.photo = ImageTk.PhotoImage(image)
+    w1.configure(image = root.photo)
+    myButton.configure(text="WHAT?")
+
+root = tk.Tk()
+w1 = tk.Label(root)
+w2 = tk.Label(root, justify=tk.LEFT , padx=50 ,pady=18 ,text="who run the world ?").pack(side="top")
+
+myButton = tk.Button(root, text="Look at the answer ...", command=show_pic)
+myButton.pack(side="top")
+w1.pack(side="top")
+
+'''def myClick():
+    myLable = Label(root, text="Look the answer ... " )
+    myLable.pack()
+    img=PhotoImage(file='answer.png')
+    Label(root,image=img)
+    Label.pack()'''
+
+
+
+
+root.mainloop()
+
+
---
+++ plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+
+'''labels = 'Python','C++','Ruby', 'Java'
+sizes = [215, 130, 245,210]
+colors = ['gold','yellowgreen','lightcoral','lightskyblue']
+explode = (0.1, 0, 0,0)
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',shadow=True , startangle=140)
+plt.axis('equal')
+plt.show()
+'''
+
+labels = 'Python','C++','Ruby', 'Java'
+sizes = [215, 130, 245,210]
+colors = ['gold','yellowgreen','lightcoral','lightskyblue']
+explode = (0, 0, 0.2,0)
+plt.pie(sizes, explode=explode,labels=labels, colors=colors, autopct='%1.1f%%',shadow=True , startangle=140)
+plt.axis('equal')
+plt.show()
+
